5f69b195e53a3e4ec5f8bcc63e842396671ee236
André <ruland@outlook.de>
2021-02-09 15:30:43 +0100

    Client Servert kram funzt soweit

---
+++ WcfChat/WcfChat.Client/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Windows;
+using WcfChat.Contracts;
+
+namespace WcfChat.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IClient
+    {
+        IServer server = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUi(false);
+
+        }
+
+       
+
+        private void LoginClick(object sender, RoutedEventArgs e)
+        {
+            var netBind = new NetTcpBinding();
+            var netAdr = "net.tcp://localhost:1";
+
+            var dcf = new DuplexChannelFactory<IServer>(this, netBind, netAdr);
+            server = dcf.CreateChannel();
+
+            server.Login(userNameTb.Text);
+        }
+
+        public void LoginResult(string msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg) || msg == "ok")
+                SetUi(true);
+            else
+                MessageBox.Show(msg);
+        }
+
+        private void SetUi(bool isLoggedIn)
+        {
+            loginBtn.IsEnabled = !isLoggedIn;
+            userNameTb.IsEnabled = !isLoggedIn;
+            
+            
+            textTb.IsEnabled = isLoggedIn;
+            logoutBtn.IsEnabled = isLoggedIn;
+        }
+
+        private void LogoutClick(object sender, RoutedEventArgs e)
+        {
+            server?.Logout();
+        }
+
+        public void LogoutResult(string msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg) || msg == "ok")
+            {
+                server = null;
+                SetUi(false);
+            }
+            else
+                MessageBox.Show(msg);
+        }
+
+        public void ShowInfo(string info)
+        {
+            MessageBox.Show(info);
+        }
+
+        public void ShowText(string text)
+        {
+            chatLb.Items.Add($"[{DateTime.Now:T}] {text}");
+        }
+
+        public void ShowUsers(IEnumerable<string> users)
+        {
+            usersLb.ItemsSource = users;
+        }
+
+        private void SendTextClick(object sender, RoutedEventArgs e)
+        {
+            if (server != null && !string.IsNullOrWhiteSpace(textTb.Text))
+            {
+                server.SendText(textTb.Text);
+                textTb.Clear();
+                textTb.Focus();
+            }
+        }
+
+
+    }
+}
--- WcfChat/WcfChat.Server/WcfChatServer.cs
+++ WcfChat/WcfChat.Server/WcfChatServer.cs
@@ -1,19 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.ServiceModel;
 using WcfChat.Contracts;
 
 namespace WcfChat.Server
 {
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public class WcfChatServer : IServer
     {
+        static Dictionary<IClient, string> users = new Dictionary<IClient, string>();
+
         public void Login(string username)
         {
             Console.WriteLine($"Login {username}");
+
+            var callback = OperationContext.Current.GetCallbackChannel<IClient>();
+            callback.ShowText($"Hallo {username}");
+
+            if (users.ContainsValue(username))
+                callback.LoginResult("Username is already in use");
+            else
+            {
+                callback.LoginResult("ok");
+                users.Add(callback, username);
+                callback.ShowUsers(users.Select(x => x.Value));
+            }
         }
 
         public void Logout()
         {
             Console.WriteLine($"Logout");
+            var callback = OperationContext.Current.GetCallbackChannel<IClient>();
+            if (users.TryGetValue(callback, out string senderName))
+            {
+                users.Remove(callback);
+                callback.LogoutResult("ok");
+            }
         }
 
         public void SendImage(Stream image)
@@ -24,6 +48,26 @@ namespace WcfChat.Server
         public void SendText(string text)
         {
             Console.WriteLine($"SendText {text}");
+
+            var callback = OperationContext.Current.GetCallbackChannel<IClient>();
+            if (users.TryGetValue(callback, out string senderName))
+            {
+                var msg = $"({senderName}) {text}";
+
+                foreach (var usr in users.ToList())
+                {
+                    try
+                    {
+                        usr.Key.ShowText(msg);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"\tError:{ex.Message}");
+                        users.Remove(callback);
+                    }
+                }
+
+            }
         }
     }
 }
