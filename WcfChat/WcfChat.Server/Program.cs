bd389289af886ea7ad469fa6cc8fbd59fb58ff34
André <ruland@outlook.de>
2021-02-09 12:02:01 +0100

    Chat Sever

---
+++ WcfChat/WcfChat.Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel;
+using WcfChat.Contracts;
+
+namespace WcfChat.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** WCF Chat Server ***");
+
+            var bind = new NetTcpBinding();
+            var host = new ServiceHost(typeof(WcfChatServer));
+
+            host.AddServiceEndpoint(typeof(IServer), bind, "net.tcp://localhost:1");
+
+            host.Open();
+            Console.WriteLine("Server wurde gestartet");
+            Console.ReadLine();
+
+            host.Close();
+            Console.WriteLine("Server wurde beendet");
+            Console.WriteLine("Ende");
+        }
+    }
+}
---
+++ WcfChat/WcfChat.Server/WcfChatServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using WcfChat.Contracts;
+
+namespace WcfChat.Server
+{
+    public class WcfChatServer : IServer
+    {
+        public void Login(string username)
+        {
+            Console.WriteLine($"Login {username}");
+        }
+
+        public void Logout()
+        {
+            Console.WriteLine($"Logout");
+        }
+
+        public void SendImage(Stream image)
+        {
+            Console.WriteLine($"SendImage");
+        }
+
+        public void SendText(string text)
+        {
+            Console.WriteLine($"SendText {text}");
+        }
+    }
+}
