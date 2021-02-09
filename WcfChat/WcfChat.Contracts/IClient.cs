eabc390902b6c7c8e96f9c25896bb067dc0feb24
André <ruland@outlook.de>
2021-02-09 11:48:05 +0100

    Contracts für den WCF Chat

---
+++ WcfChat/WcfChat.Contracts/IClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace WcfChat.Contracts
+{
+    [ServiceContract]
+    public interface IClient
+    {
+        [OperationContract(IsOneWay = true)]
+        void ShowText(string text);
+
+        [OperationContract(IsOneWay = true)]
+        void ShowUsers(IEnumerable<string> users);
+
+        [OperationContract(IsOneWay = true)]
+        void ShowInfo(string info);
+
+        [OperationContract(IsOneWay = true)]
+        void LoginResult(string msg);
+
+        [OperationContract(IsOneWay = true)]
+        void LogoutResult(string msg);
+    }
+}
